4547f96217327c36ffa3423a4e22c723550c018d
candescent-torrent-9701 <williamsmatthew333@gmail.com>
2020-05-19 03:18:08 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -39,9 +39,9 @@ end
 
 # Define your play method below
 def play(board)
-  counter = 0
-  while counter < 9
-    turn(board)
-    counter += 1
+  turns = 0
+  while turns < 10 
+  turn(board)
+  turns  +=
   end
 end
